a7d085c2ba9b2161f7e221fac669f9a15ed400cc
VadimShapovalov <kidav_21@mail.ru>
2022-07-10 10:08:01 +0300

    Нахождение максимамального из двух чисел

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Упр. 1 (Задача 2): Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Напишите первое число ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Напишите второе число ");
+
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("max = ");
+
+if(x > y)
+{
+    Console.WriteLine(x);
+}
+else
+{
+    Console.WriteLine(y);
+}
